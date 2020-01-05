0bc0963bf6930566b1fbae111ba1fdc403884aaf
wenbin huang <ben@wenbins-MacBook-Pro.local>
2020-01-04 22:30:01 -0800

    add minstack

---
+++ stack/minstack.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  4 22:28:19 2020
+
+@author: ben
+"""
+
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.minimum = []
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.stack.append(x)
+        if self.minimum:
+            self.minimum.append(min(x, self.minimum[-1]))
+        else:
+            self.minimum.append(x)
+            
+        
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+        self.minimum.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        if not self.stack:
+            return None
+        
+        return self.stack[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        if not self.minimum:
+            return None
+        
+        return self.minimum[-1]
